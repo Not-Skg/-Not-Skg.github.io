8f37095cc33d121f0af498f12eb842e6ef9820d3
Not-Skg <166406902+Not-Skg@users.noreply.github.com>
2024-04-12 15:57:23 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-// Sélectionner tous les éléments collapsible
-const collapsibles = document.querySelectorAll('.collapsible');
+  // Sélectionner tous les éléments collapsible
+  const collapsibles = document.querySelectorAll('.collapsible');
 
-// Ajouter un gestionnaire d'événements de clic à chaque élément collapsible
-collapsibles.forEach(collapsible => {
-  collapsible.addEventListener('click', function() {
-    // Fermer tous les autres collapsibles
-    collapsibles.forEach(otherCollapsible => {
-      if (otherCollapsible !== collapsible) {
-        otherCollapsible.classList.remove('active');
-        otherCollapsible.nextElementSibling.style.maxHeight = null;
+  // Ajouter un gestionnaire d'événements de clic à chaque élément collapsible
+  collapsibles.forEach(collapsible => {
+    collapsible.addEventListener('click', function() {
+      // Fermer tous les autres collapsibles
+      collapsibles.forEach(otherCollapsible => {
+        if (otherCollapsible !== collapsible) {
+          otherCollapsible.classList.remove('active');
+          otherCollapsible.querySelector('.collapsible-content').style.maxHeight = null;
+        }
+      });
+
+      // Basculer l'état actif du collapsible actuel
+      collapsible.classList.toggle('active');
+      const content = collapsible.querySelector('.collapsible-content');
+      if (content.style.maxHeight) {
+        content.style.maxHeight = null;
+      } else {
+        content.style.maxHeight = content.scrollHeight + "px";
       }
     });
-
-    // Basculer l'état actif du collapsible actuel
-    collapsible.classList.toggle('active');
-    const content = collapsible.nextElementSibling;
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
   });
-});
-          
+
 });
