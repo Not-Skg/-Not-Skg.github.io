475b159380d11dad87a7e88d8a87a3e224b6afdc
Not-Skg <166406902+Not-Skg@users.noreply.github.com>
2024-04-12 15:38:58 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,16 +2,17 @@ document.addEventListener("DOMContentLoaded", function() {
 
 // Sélectionner tous les éléments collapsible
 const collapsibles = document.querySelectorAll('.collapsible');
-let lastActiveCollapsible = null; // Ajouter une variable pour stocker le dernier collapsible actif
 
 // Ajouter un gestionnaire d'événements de clic à chaque élément collapsible
 collapsibles.forEach(collapsible => {
   collapsible.addEventListener('click', function() {
-    // Fermer le dernier collapsible actif
-    if (lastActiveCollapsible && lastActiveCollapsible !== collapsible) {
-      lastActiveCollapsible.classList.remove('active');
-      lastActiveCollapsible.nextElementSibling.style.maxHeight = null;
-    }
+    // Fermer tous les autres collapsibles
+    collapsibles.forEach(otherCollapsible => {
+      if (otherCollapsible !== collapsible) {
+        otherCollapsible.classList.remove('active');
+        otherCollapsible.nextElementSibling.style.maxHeight = null;
+      }
+    });
 
     // Basculer l'état actif du collapsible actuel
     collapsible.classList.toggle('active');
@@ -21,11 +22,7 @@ collapsibles.forEach(collapsible => {
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
-
-    // Mettre à jour la référence au dernier collapsible actif
-    lastActiveCollapsible = collapsible;
   });
 });
-
           
 });
