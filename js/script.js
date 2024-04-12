9822eff9ede926ffe77110fcf477a39fa8a6f640
Not-Skg <166406902+Not-Skg@users.noreply.github.com>
2024-04-12 15:26:02 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-var coll = document.getElementsByClassName("collapsible");
+// Sélectionner tous les éléments collapsible
+const collapsibles = document.querySelectorAll('.collapsible');
 
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
+// Ajouter un gestionnaire d'événements de clic à chaque élément collapsible
+collapsibles.forEach(collapsible => {
+  collapsible.addEventListener('click', function() {
+    // Fermer tous les autres collapsibles
+    collapsibles.forEach(otherCollapsible => {
+      if (otherCollapsible !== collapsible) {
+        otherCollapsible.classList.remove('active');
+        otherCollapsible.nextElementSibling.style.maxHeight = null;
+      }
+    });
+
+    // Basculer l'état actif du collapsible actuel
+    collapsible.classList.toggle('active');
+    const content = collapsible.nextElementSibling;
     if (content.style.maxHeight) {
       content.style.maxHeight = null;
     } else {
       content.style.maxHeight = content.scrollHeight + "px";
     }
   });
-}
+});
 
           
 });
