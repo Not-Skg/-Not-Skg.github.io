d5e47b4c9ab8b482effb0da8a7d7e2de859a46c4
Not-Skg <166406902+Not-Skg@users.noreply.github.com>
2024-04-13 12:16:37 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -25,4 +25,21 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
 
+
+// SBack to top button
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 });
