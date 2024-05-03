204aefc2a1bb746b9547fb0d12a7c1976e176862
Not-Skg <166406902+Not-Skg@users.noreply.github.com>
2024-05-03 11:41:19 +0200

    Update header.php

--- php/header.php
+++ php/header.php
@@ -1,1 +1,37 @@
 
+ <header>
+      <a href="/" class="header-logo"><img src="/img/logo.png" width ="75" ></a>
+      <a href="/" class="header-brand"> SKG </a>
+      <a href="#" class="bar"></a>
+
+      <nav>
+          <ul>
+            <li><a href="/tryhackme/"> Tryhackme </a> </li>
+            <li><a href="/cryptohack/"> Cryptohack </a> </li>
+            <li><a href="/rootme/"> Rootme </a> </li>
+            <li><a href="/Construction"><font color="olive"><i> WhoAmI </i></font></a> </li>
+          </ul>
+
+        <div class="toggle-switch-container">
+          <label class="switch">
+            <input type="checkbox" id="darkModeSwitch" />
+            <span class="slider"></span>
+          </label>
+        
+          <div id="dropdown-header" class="dropdown">
+            <button class="dropbtn"><font color="olive"><i> Code Snippet</i></font> </button>
+            <div class="dropdown-content active">
+              <a href="/Construction"><font color="olive"><i> CODE </i></font></a>
+              <a href="/Construction"><font color="olive"><i> CODE </i></font></a>
+              <a href="/Construction"><font color="olive"><i> CODE </i></font></a>
+              <a href="/Construction"><font color="olive"><i> CODE </i></font></a>
+              <a href="/Construction"><font color="olive"><i> CODE </i></font></a>
+            </div>
+          </div>
+        </div>
+
+    </nav>
+    <div class="menu-toggle">
+      <i class="fa fa-bars" aria-hidden="true"></i>
+    </div>
+  </header>
